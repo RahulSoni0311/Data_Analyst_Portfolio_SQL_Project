41aba0b09eec84e452e3297c1a69970d9d038a76
RahulSoni0311 <161428354+RahulSoni0311@users.noreply.github.com>
2026-09-05 15:02:11 +0530

    Add foreign key integrity check for fact_sales
    Added SQL query to check foreign key integrity for sales transactions.

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -1,1 +1,15 @@
+-- ============================================================================
+-- GOLD: Fact Sales
+-- ============================================================================
 
+-- Check foreign key integrity
+-- Ensure every sales transaction is linked to a valid product in the
+-- product dimension
+
+SELECT *
+FROM gold.fact_sales AS f
+LEFT JOIN gold.dim_customers AS c
+    ON f.customer_key = c.customer_key
+LEFT JOIN gold.dim_products AS p
+    ON f.product_key = p.product_key
+WHERE p.product_key IS NULL;
